fac3e50c3aeb36d94107743b840d18653895018f
Mohd Ismail Siddiqui <smile.sidd@hotmail.com>
2024-05-10 23:35:12 +0530

    package update

--- GenericSwagger/SwaggerServiceCollectionExtensions.cs
+++ GenericSwagger/SwaggerServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
